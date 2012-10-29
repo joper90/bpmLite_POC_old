f52a54f3336e3daa5b031ae79271678e438d07b0
joe90 <joper90@gmail.com>
2012-10-29 21:00:27 +0000

    Intial Commit
    Inital commit of basic framework

---
+++ designStudio/src/tests/QuickTest.java
@@ -0,0 +1,51 @@
+package tests;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.opengl.renderer.Renderer;
+
+public class QuickTest extends BasicGame{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Running");
+		 try { 
+	            Renderer.setRenderer(Renderer.VERTEX_ARRAY_RENDERER); 
+	             
+	            AppGameContainer container = new AppGameContainer(new QuickTest()); 
+	            container.setDisplayMode(800,600,false); 
+	            container.start(); 
+	        } catch (SlickException e) { 
+	            e.printStackTrace(); 
+	        } 
+	}
+	
+	public QuickTest()
+	{
+		super("QuickTest");
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ webServer/src/engine/LogicTest.java
@@ -0,0 +1,13 @@
+package engine;
+
+public class LogicTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
